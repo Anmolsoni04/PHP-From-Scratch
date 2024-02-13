5fbbb1b4af09721e52518d24945f3771125bc748
AnmolSoni0102 <anmolsoni@labourhomeindia.com>
2024-02-13 16:45:45 +0530

    php-initial-commit

---
+++ basics.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exploring PHP</title>
+</head>
+<body>
+    <div class="box">
+        Hi i am HTML
+    </div>
+    <?php
+    echo "I am php";
+    ?>
+</body>
+</html>
